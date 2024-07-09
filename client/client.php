4a359b3c1f884744380789229354b89b0da5a4fe
sultanashraf14 <sultanashraf.jobs@gmail.com>
2024-07-09 14:30:00 +0530

    New Code

--- client/client.php
+++ client/client.php
@@ -6,21 +6,9 @@ $clientid = $_GET['clientid'];
 $status = $_GET['status'];
 $username = $_GET['username'];
 
-if ($status == "redirectsComplete") {
-    $newCompleteCount == null;
-    $getLinkDataQuery = "SELECT * FROM `projects_suppliers_link` WHERE `client_id`='$clientid' AND `status`='live'";
-    $getLinkData = mysqli_query($conn, $getLinkDataQuery);
-    $getLinkDataRows = mysqli_fetch_array($getLinkData);
-    echo "<pre>";
-    print_r($getLinkDataRows);
-    $linkid = $getLinkDataRows['link_id'];
-    $completeCount = $getLinkDataRows['completes'];
-    if ($completeCount == null) {
-        $newCompleteCount = "1";
-    } else {
-        $newCompleteCount = $completeCount + 1;
-    }
-
+//Get IP Address
+function getIPAddress()
+{
     $ipaddress = '';
     if (isset($_SERVER['HTTP_CLIENT_IP']))
         $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
@@ -36,13 +24,30 @@ if ($status == "redirectsComplete") {
         $ipaddress = $_SERVER['REMOTE_ADDR'];
     else
         $ipaddress = 'UNKNOWN';
-    echo $ipaddress;
+    return $ipaddress;
+}
 
+if ($status == "redirectsComplete") {
+    $newCompleteCount == null;
+    $getLinkDataQuery = "SELECT * FROM `projects_suppliers_link` WHERE `client_id`='$clientid' AND `status`='live'";
+    $getLinkData = mysqli_query($conn, $getLinkDataQuery);
+    $getLinkDataRows = mysqli_fetch_array($getLinkData);
+    echo "<pre>";
+    print_r($getLinkDataRows);
+    $linkid = $getLinkDataRows['link_id'];
+    $completeCount = $getLinkDataRows['completes'];
+    if ($completeCount == null) {
+        $newCompleteCount = "1";
+    } else {
+        $newCompleteCount = $completeCount + 1;
+    }
+
+    $ipaddress = getIPAddress();
     $updateCompleteCountQuery = "UPDATE `projects_suppliers_link` SET `completes`='$newCompleteCount',`ipAdd`='$ipaddress' WHERE client_id='$clientid' AND status='live'";
     if ($updateResult = mysqli_query($conn, $updateCompleteCountQuery)) {
         echo "Updated..!";
         $updateCompleteStatus = "UPDATE `projects_suppliers_link` SET `status`='paused' WHERE client_id='$clientid' AND status='live'";
-        if ($updateResult = mysqli_query($conn, $updateCompleteStatus)) {
+        if ($updateStatusResult = mysqli_query($conn, $updateCompleteStatus)) {
             echo "Status Updated..!";
         }
         //header("location: https://localhost/ilika/redirectsComplete.php");
@@ -50,6 +55,32 @@ if ($status == "redirectsComplete") {
         echo "Error updating record: " . mysqli_error($conn);
     }
 } else if ($status == "redirectsTerminate") {
+    $newTerminateCount == null;
+    $getLinkDataQuery = "SELECT * FROM `projects_suppliers_link` WHERE `client_id`='$clientid' AND `status`='live'";
+    $getLinkData = mysqli_query($conn, $getLinkDataQuery);
+    $getLinkDataRows = mysqli_fetch_array($getLinkData);
+    echo "<pre>";
+    print_r($getLinkDataRows);
+    $linkid = $getLinkDataRows['link_id'];
+    $terminateCount = $getLinkDataRows['terminates'];
+    if ($terminateCount == null) {
+        $newTerminateCount = "1";
+    } else {
+        $newTerminateCount = $terminateCount + 1;
+    }
+
+    $ipaddress = getIPAddress();
+    $updateCompleteCountQuery = "UPDATE `projects_suppliers_link` SET `completes`='$newTerminateCount',`ipAdd`='$ipaddress' WHERE client_id='$clientid' AND status='live'";
+    if ($updateResult = mysqli_query($conn, $updateCompleteCountQuery)) {
+        echo "Updated..!";
+        $updateCompleteStatus = "UPDATE `projects_suppliers_link` SET `status`='paused' WHERE client_id='$clientid' AND status='live'";
+        if ($updateStatusResult = mysqli_query($conn, $updateCompleteStatus)) {
+            echo "Status Updated..!";
+        }
+        //header("location: https://localhost/ilika/redirectsComplete.php");
+    } else {
+        echo "Error updating record: " . mysqli_error($conn);
+    }
 } else if ($status == "redirectsQuotafull") {
 } else if ($status == "qualityTerminate") {
 }
