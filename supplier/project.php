4fcfd50a591dd0c4fb0fbf205860f2fd31c34242
sultanashraf14 <sultanashraf.jobs@gmail.com>
2024-07-30 09:19:52 +0530

    MC

--- supplier/project.php
+++ supplier/project.php
@@ -125,6 +125,8 @@ if ($supplierStatus == "live") {
                 $insertStatus = "INSERT INTO `projects_suppliers_link_status`
                 (`lead_id`, `p_id`, `link_id`, `project_id`, `sipplier_id`, `client_id`, `status`, `ip_address`, `username`) VALUES 
                 ('$new_lead_id','$project_id','$link_id','$project_id','$supplier_id','$client_id','$status','$ipaddress','$dbusername')";
+                echo $insertStatus;
+                exit();
                 if ($insertStatusResult = mysqli_query($conn, $insertStatus)) {
                     header("Location: " . $newUrl);
                 } else {
